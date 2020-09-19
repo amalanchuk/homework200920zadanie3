7918739113b881d79a9ab255b723ae49302b0a66
amalanchuk <artem.malanchuk@gmail.com>
2020-09-19 14:06:28 +0300

    finish

---
+++ index.js
@@ -0,0 +1,18 @@
+let credits = 23580;
+let pricePerDroid = 3000;
+let totalPrice;
+let input = prompt("Сколько дроидов вы хотите купить?");
+
+if (input === null) {
+    alert("Отменено пользователем!");
+} else {
+    let num = Number(input);
+    totalPrice = num * pricePerDroid;
+
+    if (totalPrice > credits) {
+        alert("Недостаточно средств на счету!");
+    } else {
+        let rest = credits - totalPrice;
+        alert(`Вы купили ${num} дроидов, на счету осталось ${rest} кредитов.`);
+    }
+}
